e82b8929d5723d687c043590e819eaebe10f6eef
Noushedul1 <noushedakib@gmail.com>
2021-11-05 00:03:34 +0600

    js module

---
+++ external.js
@@ -0,0 +1,15 @@
+// export const a = 10;
+// export const b = 39;
+// export function add(){
+//     console.log('noushed');
+// }
+
+// export const d = 23;
+// const f = 45;
+// export default f;
+
+export const m = 34;
+export default function add(a,b){
+    console.log(a+b);
+}
+
---
+++ index.js
@@ -0,0 +1,12 @@
+// import {a as varA ,b,add} from './external.js';
+// console.log(b);
+// console.log(varA);
+// add();
+
+// import hello,{d} from './external.js';
+// console.log(hello);
+// console.log(d);
+
+import my,{m} from './external.js';
+my(3,4);
+console.log(m);
